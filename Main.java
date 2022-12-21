c1ae3221a2aee4844894b458178ff556b512b02d
Rafal <105854518+Gladziu@users.noreply.github.com>
2022-12-21 23:09:36 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+//Konwerter jednostek długości, masy, czasu, temperatury
+public class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("KONWERTER JEDNOSTEK - dlugosc, masa, czas, temperatura");
+        
+        //input value
+        System.out.print("1. Podaj wartosc: ");
+        double value = scan.nextDouble();
+        scan.nextLine(); //scanning end of line
+        
+        //input unit
+        System.out.print("2. Podaj jednostke: ");
+        String unit = scan.nextLine();
+        
+        //check if unit is correct and if input value is correct
+        try {
+            CorrectUnit check = new CorrectUnit(value, unit); 
+            check.check();
+        } catch (IllegalArgumentException ex) {
+            System.out.println(ex);
+            main(args);
+        }
+        
+        ValueAndUnit valueAndUnit = new ValueAndUnit(value, unit);
+
+        //Convert to main values(m,kg,s,C)
+        ConvertLenght meters = valueAndUnit.toMainValueLenght();
+        ConvertWeight kilograms = valueAndUnit.toMainValueWeight();
+        ConvertTime seconds = valueAndUnit.toMainValueTime();
+        ConvertTemperature celcius = valueAndUnit.toMainValueTemperature();
+
+        //convert and display finall results
+        meters.convertAndDisplay();
+        kilograms.convertAndDisplay();
+        seconds.convertAndDisplay();
+        celcius.convertAndDisplay();
+    }
+}
