3de68cd3a34c777203b0019f4b8ced609093bb3d
Rafal <105854518+Gladziu@users.noreply.github.com>
2023-01-23 18:37:47 +0100

    Create ConvertLenght.java

---
+++ Project/src/pl/poznan/put/conversion/ConvertLenght.java
@@ -0,0 +1,20 @@
+//Lenght converter, finall results
+public class ConvertLenght implements ConvertAndDisplay{
+    private double valueToConvert;
+    public ConvertLenght(double mainValue){
+        valueToConvert = mainValue;
+    }
+    
+    @Override
+    public void convertAndDisplay(){
+        if (valueToConvert >= 0){
+            double mm = valueToConvert * 1000;
+            double cm = valueToConvert * 100;
+            double dm = valueToConvert * 10;
+            double m = valueToConvert;
+            double km = valueToConvert / 1000;
+            System.out.println(mm + "[mm], " + cm + "[cm], " + dm + "[dm], " + m + "[m], " + km + "[km]");
+        }
+       
+    }
+}
