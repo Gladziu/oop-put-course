ceb5b10173deadd39655ec6fbe12837673d6c872
Rafal <105854518+Gladziu@users.noreply.github.com>
2023-01-23 18:38:32 +0100

    Add files via upload

---
+++ Project/src/pl/poznan/put/conversion/ConvertTemperature.java
@@ -0,0 +1,19 @@
+//Temperature converter, finall results
+public class ConvertTemperature implements ConvertAndDisplay{
+    private double valueToConvert;
+    private Boolean endOfLoop;
+    public ConvertTemperature(double mainValue, Boolean endOfLoop){
+        valueToConvert = mainValue;
+        this.endOfLoop = endOfLoop;
+    }
+
+    @Override
+    public void convertAndDisplay(){
+        if (endOfLoop == true){
+            double c = valueToConvert;
+            double f = ((valueToConvert * 9) / 5) + 32;
+            double k = valueToConvert + 273;
+            System.out.println(c + "[C], " + f + "[F], " + k + "[K]");
+        }
+    }
+}
---
+++ Project/src/pl/poznan/put/conversion/ConvertWeight.java
@@ -0,0 +1,19 @@
+//Weight converter, finall results
+public class ConvertWeight implements ConvertAndDisplay{
+    private double valueToConvert;
+    public ConvertWeight(double mainValue){
+        valueToConvert = mainValue;
+    }
+    
+    @Override
+    public void convertAndDisplay(){
+        if (valueToConvert >= 0){
+            double mg = valueToConvert * 1000000;
+            double g = valueToConvert * 1000;
+            double dkg = valueToConvert * 100;
+            double kg = valueToConvert;
+            double t = valueToConvert / 1000;
+            System.out.println(mg + "[mg], " + g + "[g], " + dkg + "[dkg], " + kg + "[kg], " + t + "[t]");
+        }
+    }
+}
